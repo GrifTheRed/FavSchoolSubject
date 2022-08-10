ab27d706074d6504decbef14dbb443c39f75ab37
Derek Griffin <griffinderek6@gmail.com>
2022-08-10 13:24:59 -0600

    favorite school subject

---
+++ FavSchoolSubject/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FavSchoolSubject
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("What is your favorite school subject?");
+
+            string subject = Console.ReadLine();
+
+            switch(subject)
+            {
+                case "math":
+                    Console.WriteLine("I hate math!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science was my favorite subject");
+                    break;
+
+                case "english":
+                    Console.WriteLine("english was close to being my favorite along with science");
+                    break;
+
+                case "art":
+                    Console.WriteLine("I did not care for art although I really enjoyed pottery");
+                    break;
+
+                case "gym":
+                    Console.WriteLine("everyone liked gym class, come on");
+                    break;
+
+                case "defualt":
+                    Console.WriteLine("didn't have that at my school");
+                    break;
+            }
+
+        }
+    }
+}
+
